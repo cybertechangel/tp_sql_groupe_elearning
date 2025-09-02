40894412daa292a65135848ca28a2c3bcb3d13ab
cybertechangel <nelnihadnel@gmail.com>
2025-09-02 16:32:19 +0200

    modif

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ import 'dotenv/config';
 import express from 'express';
 import userRoutes from './routes/userRoutes.js';
 import reviewRoutes from './routes/reviewRoutes.js';
+import courseRoutes from './routes/courseRoutes.js';
+import enrollmentRoutes from './routes/enrollmentRoutes.js';
 
 const app = express();
 app.use(express.json());
 
 app.use('/users', userRoutes);
 app.use('/reviews', reviewRoutes);
+app.use("/enrollments", enrollmentRoutes);
+app.use("/courses", courseRoutes);
 
 app.use((req, res) => res.status(404).json({ error: 'Non trouvé' }));
 
